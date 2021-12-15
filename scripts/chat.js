445f3ebc22690af85556e87c02c3aa3a34741391
Kevin Lamb <kevlamb@seas.upenn.edu>
2021-12-14 21:33:39 -0500

    frontend for a chat. Messages appear in divs that alternate color, chat autoscrolls to the bottom when a new message is received. Added a navbar to /chat page that could be used for more information as well

--- scripts/chat.js
+++ scripts/chat.js
@@ -4,6 +4,7 @@ var username = null;
 var chat = null;
 var messageInput = null;
 var chatBox = null;
+var num_messages = 0;
 
 /*
 Called when you first load the page. Initializes importsant document elements as variables
@@ -15,7 +16,7 @@ $(document).ready(function() {
 	messageInput = document.getElementById('new_message');
 	chatBox = document.getElementById('chat-box');
 	username = document.getElementById('username').innerHTML;
-	
+	console.log(chat);
 	chat.addEventListener('submit', function(event) {
 		event.preventDefault();
 		send_message(chatid, messageInput.value);
@@ -29,12 +30,18 @@ Adds the new message to the chat if the chatid matches the current opened chat
 */
 socket.on('chat-message', function(data) {
 	if(chatid == data.chatid) {
-		console.log("received message"+data.message);
+		//add the message to the bottom of the chat log
 		appendMessage(data.message, data.user);
+		
+		//autoscroll to the bottom of the chat if not there already
+		if (chatBox.scrollHeight - chatBox.offsetHeight - chatBox.scrollTop > 1) {
+		chatBox.scrollTop = chatBox.scrollHeight;
+		}
 	}
 });
 
 function appendMessage(message, username) {
+	/*
 	let table = document.getElementById('display');
 	var content = table.insertRow();
 	var contentcell = content.insertCell();
@@ -42,8 +49,25 @@ function appendMessage(message, username) {
 	var postercell = poster.insertCell();
 	contentcell.innerText = message;
 	postercell.innerText = username;
-	//messageElm.innerText = message;
-	//chatBox.append(messageElm);
+	*/
+
+	console.log(num_messages);
+	
+	let messagecontainer = document.createElement("div");
+	messagecontainer.setAttribute("id", "message-container-"+(num_messages % 2));
+		
+	let messageusername = document.createElement("div");
+	messageusername.setAttribute("id", "message-username");
+	messageusername.innerHTML = username;
+	messagecontainer.append(messageusername);
+		
+	let messagecontent = document.createElement("div");
+	messagecontent.setAttribute("id", "message-content");
+	messagecontent.innerHTML = message;
+	messagecontainer.append(messagecontent);
+		
+	chatBox.append(messagecontainer);
+	num_messages++;
 	
 };
 
@@ -75,7 +99,7 @@ async function populate_chat_onLoad(chatid){
 	console.log(chatid);
 	let messages = (await loadData(chatid)).Items;
 	for (let i = 0; i < messages.length; i++) {
-		console.log("found a message");
+		/*console.log("found a message");
 		var content = table.insertRow();
 		var contentcell = content.insertCell();
 		var poster = table.insertRow();
@@ -83,6 +107,24 @@ async function populate_chat_onLoad(chatid){
 		
 		contentcell.innerHTML = messages[i]["message"].S;
 		poster.innerHTML = messages[i]["username"].S;
+		*/
+		let messagecontainer = document.createElement("div");
+		messagecontainer.setAttribute("id", "message-container-"+(i%2));
+		
+		let messageusername = document.createElement("div");
+		messageusername.setAttribute("id", "message-username");
+		messageusername.innerHTML = messages[i]["username"].S;
+		messagecontainer.append(messageusername);
+		
+		let messagecontent = document.createElement("div");
+		messagecontent.setAttribute("id", "message-content");
+		messagecontent.innerHTML = messages[i]["message"].S;
+		messagecontainer.append(messagecontent);
+		
+		chatBox.append(messagecontainer);
+		num_messages++;
+		
+		
 	}
 };
 
@@ -94,9 +136,25 @@ async function send_message(chatid, message) {
 	//console.log("sending the following message to chat with id " +chatid+":");
 	//console.log($("#new_message").val());
 	appendMessage(message, username);
+	
+	//autoscroll to the bottom of the chat if not there already
+	if (chatBox.scrollHeight - chatBox.offsetHeight - chatBox.scrollTop > 1) {
+		chatBox.scrollTop = chatBox.scrollHeight;
+	}
+	
 	socket.emit('send-chat-message', {message: message, username: username, chatid: chatid});
 	$.post('/sendmessage', {chatid: chatid, message: message}, function(data) {
 		console.log(data);
 	});
 	messageInput.value = '';
 }
+
+//code for handling chat autoscrolling to most recent messages
+function isFeedAtBottom() {
+    return (this.chatBox.offsetHeight+this.chatBox.scrollTop)===this.chatBox.scrollHeight;
+};
+
+function scrollFeedToBottom() {
+    this.chatBox.scrollTop = this.chatBox.scrollHeight;
+};
+
